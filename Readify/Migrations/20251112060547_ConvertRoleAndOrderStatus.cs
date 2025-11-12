494fd6881de08d7e5ad49f1019b164774be91088
Vk-Viman <Electrusgreen@gmail.com>
2025-11-12 11:42:17 +0530

    Add ConvertRoleAndOrderStatus migration

---
+++ Readify/Migrations/20251112060547_ConvertRoleAndOrderStatus.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Readify.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConvertRoleAndOrderStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateDelivered",
+                table: "Orders",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Orders",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateDelivered",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Orders");
+        }
+    }
+}
